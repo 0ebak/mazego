1bde6f7515c6f86978cd3a0f3021d78f80d14594
b1art <art.burunoff@yandex.ru>
2021-10-01 10:40:34 +0300

    Погода

--- main/views.py
+++ main/views.py
@@ -1,8 +1,23 @@
+import requests
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    appid = "b2b4026430f458f959496f2c559436d8"
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = "London"
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+
+    context = {"info": city_info}
+
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
