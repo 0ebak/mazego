7342249362a63ad4d8b58a69dbc8b0f6d4592715
0ebak <bakanach666@gmail.com>
2021-11-05 21:11:31 +0300

    final parsing afisha.yandex.ru but there is a json problem which appears only on server. running views.py it works

--- main/views.py
+++ main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from translate import Translator
 from datetime import datetime
 import pytz
-
+from bs4 import BeautifulSoup
+import json
 
 def index(request):
     appid = "b2b4026430f458f959496f2c559436d8"
@@ -42,13 +43,30 @@ def game(request):
 def get_picture():
     tz = pytz.timezone('Europe/Moscow')
     current_datetime = datetime.now(tz)
-
+    print(current_datetime.hour)
     if current_datetime.hour > 6 and current_datetime.hour < 12:
         return "morning.jpg"
     elif current_datetime.hour > 12 and current_datetime.hour < 18:
-        return "day.jpg"
+        return "day12.jpg"
     elif current_datetime.hour > 18:
         return("evening.jpg")
     else:
         return "night.jpg"
 
+
+def get_cinema(city):
+    url = 'https://afisha.yandex.ru/'+city.lower()+'/selections/all-events-cinema'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    cinemas = soup.find_all('script', type="application/ld+json")
+    cinemas = json.loads(list(cinemas)[0].text)
+    for cinema in cinemas:
+        name = cinema['name']
+        try:
+            rating = cinema['aggregateRating']['ratingValue']
+            print(name, '-', rating)
+        except KeyError:
+            print(name, '- no rating', )
+
+
+get_cinema('Moscow')
