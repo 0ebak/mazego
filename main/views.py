426b32cf864e69a0d5253d7aedeb2a1ca07b7ac7
0ebak <bakanach666@gmail.com>
2021-11-05 21:11:31 +0300

    123

--- main/views.py
+++ main/views.py
@@ -9,8 +9,6 @@ def index(request):
     appid = "b2b4026430f458f959496f2c559436d8"
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
-    time_ref = "night.jpg"
-
     city = "London"
     if request.method == 'POST':
         city = request.POST['city']
