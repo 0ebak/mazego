dc183f32cd49b805e1bd39a653156e16113a8f67
0ebak <bakanach666@gmail.com>
2021-11-20 01:27:35 +0300

    yandex news parsing

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import pytz
 from bs4 import BeautifulSoup
 import json
+import re
 
 def index(request):
     appid = "b2b4026430f458f959496f2c559436d8"
@@ -68,3 +69,23 @@ def get_cinema(city):
         except KeyError:
             print(name, '- no rating', )
 
+
+def get_news(city, start):
+
+    if start:
+        url = 'https://yandex.ru/news/region/'+city.lower().replace('-', '_').replace(' ', '_')
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        list_of_pictures = soup.find_all("div", class_="mg-card__media-block mg-card__media-block_type_image")[:4]
+        list_of_links = soup.find_all(class_="mg-card__text")[:4]
+        list_of_titles = soup.find_all("h2", class_="mg-card__title")[1:5]
+        news_list= []
+        try:
+            for i in 0, 1, 2, 3:
+                picture= re.search(r"(?<=url\().*(?=\))", list_of_pictures[i]["style"]).group(0)
+                title= list_of_titles[i].text
+                link=  list_of_links[i].find('a')['href']
+                news_list.append({'picture': picture, 'title': title, 'link': link})
+        except IndexError:
+            print("CAPTCHA")
+get_news('moscow', False) # change False to True and it'll work
