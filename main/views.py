aa96350cea10a2ee2a477e5df1f64af5ae07fba1
0ebak <bakanach666@gmail.com>
2021-11-26 02:39:57 +0300

    cbr.ru parsing and trying to fix afisha parsing

--- main/views.py
+++ main/views.py
@@ -4,8 +4,9 @@ from translate import Translator
 from datetime import datetime
 import pytz
 from bs4 import BeautifulSoup
-import json
+import ast
 import re
+
 
 def index(request):
     appid = "b2b4026430f458f959496f2c559436d8"
@@ -40,6 +41,7 @@ def about(request):
 def game(request):
     return render(request, 'main/game.html')
 
+
 # функция для изменения фона в зависимости от времени
 def get_picture():
     tz = pytz.timezone('Europe/Moscow')
@@ -50,42 +52,58 @@ def get_picture():
     elif current_datetime.hour > 12 and current_datetime.hour < 18:
         return "day12.jpg"
     elif current_datetime.hour > 18:
-        return("evening.jpg")
+        return ("evening.jpg")
     else:
         return "night.jpg"
 
 
 def get_cinema(city):
-    url = 'https://afisha.yandex.ru/'+city.lower()+'/selections/all-events-cinema'
+    false = False
+    true = True
+    none = None
+    url = 'https://afisha.yandex.ru/' + city.lower() + '/selections/all-events-cinema'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
-    cinemas = soup.find_all('script', type="application/ld+json")
-    cinemas = json.loads(list(cinemas)[0].text)
-    for cinema in cinemas:
-        name = cinema['name']
-        try:
-            rating = cinema['aggregateRating']['ratingValue']
-            print(name, '-', rating)
-        except KeyError:
-            print(name, '- no rating', )
+    cinemas = soup.find_all('div', class_="i-react event-card-react i-bem")
+    print(ast.literal_eval(cinemas[0].get('data-bem')))
+#get_cinema("saint-petersburg")
 
 
 def get_news(city, start):
-
     if start:
-        url = 'https://yandex.ru/news/region/'+city.lower().replace('-', '_').replace(' ', '_')
+        url = 'https://yandex.ru/news/region/' + city.lower().replace('-', '_').replace(' ', '_')
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'lxml')
         list_of_pictures = soup.find_all("div", class_="mg-card__media-block mg-card__media-block_type_image")[:4]
         list_of_links = soup.find_all(class_="mg-card__text")[:4]
         list_of_titles = soup.find_all("h2", class_="mg-card__title")[1:5]
-        news_list= []
+        news_list = []
         try:
             for i in 0, 1, 2, 3:
-                picture= re.search(r"(?<=url\().*(?=\))", list_of_pictures[i]["style"]).group(0)
-                title= list_of_titles[i].text
-                link=  list_of_links[i].find('a')['href']
+                picture = re.search(r"(?<=url\().*(?=\))", list_of_pictures[i]["style"]).group(0)
+                title = list_of_titles[i].text
+                link = list_of_links[i].find('a')['href']
                 news_list.append({'picture': picture, 'title': title, 'link': link})
         except IndexError:
             print("CAPTCHA")
-get_news('moscow', False) # change False to True and it'll work
+
+
+get_news('moscow', False)  # change False to True and it'll work
+
+
+def get_currency(code_list):
+    url = 'https://www.cbr.ru/currency_base/daily/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    list_of_currency = soup.find_all("tr")
+    final_list_of_currency = {}
+    for currency in list_of_currency:
+        current_code = list(currency)[3].text
+        if current_code in code_list:
+            quantity = list(currency)[5].text
+            name = list(currency)[7].text
+            value = list(currency)[9].text
+            final_list_of_currency[current_code] = [quantity, name, value]
+    print(final_list_of_currency)
+
+# get_currency(["USD", "EUR","KZT"])
