2901926995d59624dee0db22e73cc2c7b030d4c8
0ebak <bakanach666@gmail.com>
2021-12-17 11:30:05 +0300

    -

--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import pytz
 from bs4 import BeautifulSoup
 import json
+import re
+
 
 def index(request):
     appid = "b2b4026430f458f959496f2c559436d8"
@@ -39,6 +41,7 @@ def about(request):
 def game(request):
     return render(request, 'main/game.html')
 
+
 # функция для изменения фона в зависимости от времени
 def get_picture():
     tz = pytz.timezone('Europe/Moscow')
@@ -61,10 +64,63 @@ def get_cinema(city):
     cinemas = soup.find_all('script', type="application/ld+json")
     cinemas = json.loads(list(cinemas)[0].text)
     for cinema in cinemas:
-        name = cinema['name']
-        try:
-            rating = cinema['aggregateRating']['ratingValue']
-            print(name, '-', rating)
-        except KeyError:
-            print(name, '- no rating', )
+        ivent = json.loads(cinema.get('data-bem'))['event-card-react']['props']
+        if ivent['type'] == 'cinema':
+            list_of_cinemas.append(ivent)
+
+    try:
+        finish_list = []
+        for i in 0, 1, 2:
+            try:
+                finish_list.append({'image': list_of_cinemas[i]['image']['url'], 'title': list_of_cinemas[i]['title'],
+                                        'description': list_of_cinemas[i]['argument'], 'rating': list_of_cinemas[i]['rating']['value']})
+            except TypeError:
+                finish_list.append({'image': list_of_cinemas[i]['image']['url'], 'title': list_of_cinemas[i]['title'],
+                                    'description': list_of_cinemas[i]['argument'],
+                                    'rating': 'No'})
+
+        return finish_list
+    except IndexError:
+        print("\nCAPTCHA at cinema\n")
+        return [{'image': 'image', 'title': 'title', 'description': 'argument', 'rating': 'rating'}]
+
+
+def get_news(city):
+    url = 'https://yandex.ru/news/region/' + city
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    list_of_pictures = soup.find_all("div", class_="mg-card__media-block mg-card__media-block_type_image")[:4]
+    list_of_pictures = list_of_pictures[:4]
+    list_of_links = soup.find_all(class_="mg-card__text")[:4]
+    list_of_titles = soup.find_all("h2", class_="mg-card__title")[1:5]
+    news_list = []
+
+    try:
+        for i in 0, 1, 2, 3:
+            picture = re.search(r"(?<=url\().*(?=\))", list_of_pictures[i]["style"]).group(0)
+            title = list_of_titles[i].text
+            link = list_of_links[i].find('a')['href']
+            news_list.append({'picture': picture, 'title': title, 'link': link})
+        return news_list
+
+    except IndexError:
+        print("\nCAPTCHA at news\n")
+        return [{'picture': '', 'title': '', 'link': ''}]
+
+
+def get_currency(code_list):
+    url = 'https://www.cbr.ru/currency_base/daily/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    list_of_currency = soup.find_all("tr")
+    final_list_of_currency = []
+    for currency in list_of_currency:
+        current_code = list(currency)[3].text
+        if current_code in code_list:
+            quantity = list(currency)[5].text
+            name = list(currency)[7].text
+            value = list(currency)[9].text
+            final_list_of_currency.append({'current_code': current_code, 'quantity': quantity, 'name': name,
+                                           'value': value})
+    return final_list_of_currency
 
